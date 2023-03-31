267c9cf3eeb47caf97821e9822b2756158a79e10
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-31 16:27:46 +0530

    add task and expiry date functionality added

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,12 +1,50 @@
-import { useState } from 'react'
-import Button from '../Button/Button'
+import { useEffect, useState } from 'react'
 import emptyListImg from '../../assets/empty-list.png'
 import './Task.css'
 
-export default function Task() {;
-    const [list, setList] = useState([
-       
-    ]);
+export default function Task() {
+
+    const [showInput, setShowInput] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [list, setList] = useState(JSON.parse(localStorage.getItem('todo-list')));
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyPress);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [inputValue, showInput]);
+
+    const addTask = () => {
+        setShowInput(!showInput);
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Escape' && showInput === true) {
+            setShowInput(false);
+        }
+        if (event.key === 'Enter' && showInput === true) {
+            if (inputValue === '') {
+                alert('Please enter a value');
+            } else {
+
+                if (localStorage.getItem("expiry-date") === null) {
+                    localStorage.setItem("expiry-date", new Date().toLocaleDateString().toString())
+                }
+                const todoList = JSON.parse(localStorage.getItem('todo-list')) || [];
+                const duplicate = todoList.find((item) => item.text === inputValue);
+                if (duplicate) {
+                    alert('This task already exists');
+                } else {
+                    const newTodoList = [...todoList, { text: inputValue, checked: false }];
+                    localStorage.setItem('todo-list', JSON.stringify(newTodoList));
+                    setList([...todoList, { text: inputValue, checked: false }])
+                    setInputValue('');
+                }
+            }
+        }
+    };
 
     const handleCheckboxChange = (index, checked) => {
         const newList = [...list];
@@ -16,11 +54,11 @@ export default function Task() {;
         };
         setList(newList);
     };
-    console.log(list.length)
+
     return (
         <>
             <div className="task-list-container">
-                {list.length ? (list.map((item, index) => (
+                {list !== null ? (list.map((item, index) => (
                     <div className='task' key={index}>
                         <div className={`task-msg ${item.checked ? 'checked' : ''}`}>
                             {item.text}
@@ -32,13 +70,30 @@ export default function Task() {;
                         />
                     </div>
                 ))
-                ):(<div className='empty-list-img'>
-                    <img src={emptyListImg} alt='Empty list'/> 
-                        <div className='empty-text'>A fresh start</div>
-                        <div className='empty-sub-text'>Anything to add ?</div>
-                        </div>)}
+                ) : (<div className='empty-list-img'>
+                    <img src={emptyListImg} alt='Empty list' />
+                    <div className='empty-text'>A fresh start</div>
+                    <div className='empty-sub-text'>Anything to add ?</div>
+                </div>)}
             </div>
-            <Button />
+            {showInput ? (
+                <div>
+                    <input
+                        type="text"
+                        className="input-size"
+                        placeholder='Press "Esc" to exit'
+                        autoFocus
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                    />
+                </div>
+            ) : (
+                <div className="add-todo-btn-container">
+                    <button className="add-todo-button" onClick={() => addTask()}>
+                        +
+                    </button>
+                </div>
+            )}
         </>
     );
 }
